6d511dda2db820536e33adb4aefe63700ce11305
tox-in <habimanat78@gmail.com>
2024-01-14 10:50:23 +0200

    new controller for checkout

---
+++ src/controllers/checkout.controller.js
@@ -0,0 +1,19 @@
+/**
+ * @license Apache-2.0
+ * *@copyright 2024 tonyherve
+  */
+
+'use strict';
+
+const checkout = async(req,res) => {
+
+    try {
+        const { amount } = req.body;
+        
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+module.exports = { checkout }
