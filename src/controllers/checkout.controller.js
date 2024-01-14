7f36a0bb04931b47945937723fe7122525127265
tox-in <habimanat78@gmail.com>
2024-01-14 12:35:41 +0200

    feat: new controller changes

--- src/controllers/checkout.controller.js
+++ src/controllers/checkout.controller.js
@@ -5,11 +5,32 @@
 
 'use strict';
 
+/**
+ *  custom modules
+ */
+
+const paymentApi = require('../api/payment.api');
+
 const checkout = async(req,res) => {
 
     try {
         const { amount } = req.body;
+
+        const invoice = await paymentApi.createInvoice(amount);
+
+        console.log(invoice);
+
+        res.json(invoice);
         
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+const callback = async(req,res) => {
+    try {
+        console.log(req.body);
     } catch (error) {
         console.error(error);
         throw error;
