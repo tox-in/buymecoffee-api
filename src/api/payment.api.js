abd4a1f2df796595b2b4e520bef03f7ee748be72
tox-in <habimanat78@gmail.com>
2024-01-14 13:32:41 +0200

    feat: new api changes

--- src/api/payment.api.js
+++ src/api/payment.api.js
@@ -5,3 +5,52 @@
 
 'use strict';
 
+/**
+ * node modules
+ */
+
+const axios = require('axios').default;
+const crypto = require('crypto');
+require('dotenv').config();
+
+const cryptomus = axios.create({
+  baseURL: 'https://api.cryptomus.com/v1'
+});
+
+const DEFAULT_CURRENCY = 'USD';
+
+const createInvoice = async (amount) => {
+  try {
+
+    const data = {
+      amount:amount.toString(),
+      currency:DEFAULT_CURRENCY,
+      orderId:crypto.randomBytes(12).toString('hex'),
+      url_callback: 'https://buymecoffee-api.onrender.com/checkout/callback',
+      url_success:'https://www.buymecoffee-api.onrender.com',
+      lifetime: 300
+    }
+
+    const sign = crypto
+    .createHash('md5')
+    .update(Buffer.from(JSON.stringify(data)).toString('base64') + process.env.PAYMENT_API_KEY)
+    .digest("hex");
+
+    const headers = {
+      merchant: process.env.MERCHANT_ID,
+      sign
+    }
+
+    const response = await cryptomus.post('/payment', data, {
+      headers
+    });
+
+    return response.data;
+
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+module.exports = { createInvoice };
