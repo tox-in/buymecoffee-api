41f8539c8b16f52b08a0004c26394774f9a4139a
tox-in <habimanat78@gmail.com>
2024-03-07 22:15:44 +0200

    initial changes

---
+++ public/js/script.js
@@ -0,0 +1,28 @@
+/**
+ *  @license Apache-2.0
+ * @copyright 2024 tox-in(tony herve)
+ */
+
+'use strict';
+
+(function() {
+/**
+ * counter
+ */
+
+const /** {HTMLElement} */ $decrementBtn = document.querySelector('[data-decrement-btn]');
+const /** {HTMLElement} */ $incrementBtn = document.querySelector('[data-increment-btn]');
+const /** {HTMLElement} */ $counterField = document.querySelector('[data-counter-field]');
+const /** {HTMLElement} */ $total = document.querySelector('[data-total]');
+const /** {Number} */ minValue = 1;
+const /** {Number} */ maxValue = 999;
+
+/**
+ * increment
+ */
+$incrementBtn.addEventListener('click', function () {
+    const currentValue = $counterField.value;
+    if (currentValue < maxValue) $counterField.value = Number(currentValue) +1;
+    updateTotal.call($counterField)
+});
+})();
